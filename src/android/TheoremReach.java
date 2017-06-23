16e682ef28a701b2781249f250be8ca8b908c9a2
MobileMaster <mobilemaster128@gmail.com>
2017-06-22 22:56:59 -0700

    first

--- src/android/TheoremReach.java
+++ src/android/TheoremReach.java
@@ -14,15 +14,15 @@ public class TheoremReach extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
+        if (action.equals("initWithApiKeyAndUserIdAndActivityContext")) {
             String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            this.initWithApiKeyAndUserIdAndActivityContext(message, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
+    private void initWithApiKeyAndUserIdAndActivityContext(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
--- www/TheoremReach.js
+++ www/TheoremReach.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "TheoremReach", "coolMethod", [arg0]);
+exports.initWithApiKeyAndUserIdAndActivityContext = function(arg0, success, error) {
+    exec(success, error, "TheoremReach", "initWithApiKeyAndUserIdAndActivityContext", [arg0]);
 };
